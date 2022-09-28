7e70be9bd6583b3be011839fe0a7e9486b58136e
Anya_Popova <limpopova218@gmail.com>
2022-09-28 16:58:08 -0400

    rectangle

--- cherep.py
+++ cherep.py
@@ -3,4 +3,8 @@ wn = turtle.Screen()        # creates a graphics window
 alex = turtle.Turtle()      # create a turtle named alex
 alex.forward(150)           # tell alex to move forward by 150 units
 alex.left(90)               # turn by 90 degrees
-alex.forward(75)            # complete the second side of a rectangle
+alex.forward(175)            # complete the second side of a rectangle
+alex.left(90)
+alex.forward(150)
+alex.left(90)
+alex.forward(175)
